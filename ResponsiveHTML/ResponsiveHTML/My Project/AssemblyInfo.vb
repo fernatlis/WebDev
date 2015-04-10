b06a612bff63f29bcdb454ecfca72d043c1b688e
fernatlis <fernandogarcia28@msn.com>
2015-04-10 14:28:05 +1000

    Initial add

---
+++ ResponsiveHTML/ResponsiveHTML/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("ResponsiveHTML")> 
+<Assembly: AssemblyDescription("Responsive HTML driven by jQuery")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ResponsiveHTML")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3b3f26f2-45cd-46a4-beb8-99b22fc7d169")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.1.0")> 
+<Assembly: AssemblyFileVersion("1.0.1.0")> 
